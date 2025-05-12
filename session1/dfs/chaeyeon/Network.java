2cd89b7f809b9cbfc17de3a12374497704d943b4
inha0319@naver.com <inha031913@gmail.com>
2025-05-13 08:56:38 +0900

    fix : 네트워크 (프로그래머스 - 43162)

--- session1/dfs/chaeyeon/Network.java
+++ session1/dfs/chaeyeon/Network.java
@@ -1,24 +1,25 @@
 class Solution {
     static int cnt = 0;
+    static boolean[] visited;
 
     public int solution(int n, int[][] computers) {
-        boolean[] visited = new boolean[n];
+        visited = new boolean[n];
 
-        for (int i=0; i<n;i++){
-            if(!visited[i]){
-                dfs(n, computers, visited, i);
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(n, computers, i);
                 cnt++;
             }
         }
         return cnt;
     }
 
-    public void dfs(int n, int[][] computers, boolean[] visited, int current){
+    public void dfs(int n, int[][] computers, int current) {
         visited[current] = true;
 
-        for(int i=0;i<n;i++){
-            if (computers[current][i] == 1 && !visited[i]){
-                dfs(n, computers, visited, i);
+        for (int i = 0; i < n; i++) {
+            if (computers[current][i] == 1 && !visited[i]) {
+                dfs(n, computers, i);
             }
         }
     }
