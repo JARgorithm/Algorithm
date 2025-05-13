f943744972f183ff9e53a33d9e5c6d65c6cad2fc
Ga0Lee <leegy0419@gmail.com>
2025-05-14 02:55:46 +0900

    feat: ⭕️ 벽 부수고 이동하기 문제 풀이 - 시간 초과 해결

--- session1/bfs/gayeong/BreakWallAndMove.java
+++ session1/bfs/gayeong/BreakWallAndMove.java
@@ -10,55 +10,57 @@ public class BreakWallAndMove {
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
     static int n, m;
-    static int[][] visited;
+    static int[][][] visited;
     static int[][] map;
     static int result = Integer.MAX_VALUE;
 
     public int solution(int[][] map) {
-        map = map;
         n = map.length;
         m = map[0].length;
 
-        breakWall();
+        move();
 
         return result;
     }
 
-    private void breakWall() {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (map[i][j] == 1) {
-                    map[i][j] = 0;
-                    result = Math.min(result, move());
-                    map[i][j] = 1;
-                }
-            }
-        }
-    }
-
     private int move() {
         Queue<Node> queue = new LinkedList<>();
-        visited = new int[n][m];
+        visited = new int[n][m][2];
 
-        queue.offer(new Node(0, 0));
-        visited[0][0] = 1;
+        queue.offer(new Node(0, 0, 0));
+        visited[0][0][0] = 1;
 
         while (!queue.isEmpty()) {
             Node current = queue.poll();
+            int x = current.x;
+            int y = current.y;
+            int broken = current.broken;
 
             for (int i = 0; i < 4; i++) {
-                int nx = current.x + dx[i];
-                int ny = current.y + dy[i];
+                int nx = x + dx[i];
+                int ny = y + dy[i];
 
-                if (nx >= 0 && nx < n && ny >= 0 && ny < m && visited[nx][ny] == 0 && map[nx][ny] == 0) {
-                    queue.offer(new Node(nx ,ny));
-                    visited[nx][ny] = visited[current.x][current.y] + 1;
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+
+                if (map[nx][ny] == 0 && visited[nx][ny][broken] == 0) {
+                    visited[nx][ny][broken] = visited[x][y][broken] + 1;
+                    queue.offer(new Node(nx, ny, broken));
+                }
+
+                if (map[nx][ny] == 1 && broken == 0 && visited[nx][ny][1] == 0) {
+                    visited[nx][ny][1] = visited[x][y][broken] + 1;
+                    queue.offer(new Node(nx, ny, 1));
                 }
             }
         }
 
-        int dist = visited[n - 1][m - 1];
-        return (dist == 0 ? Integer.MAX_VALUE : dist);
+        int noBreak = visited[n-1][m-1][0];
+        int didBreak = visited[n-1][m-1][1];
+
+        if (noBreak == 0) noBreak = Integer.MAX_VALUE;
+        if (didBreak == 0) didBreak = Integer.MAX_VALUE;
+
+        return Math.min(noBreak, didBreak);
     }
 
     public static void main(String[] args) throws IOException {
@@ -87,9 +89,11 @@ public class BreakWallAndMove {
 class Node {
     int x;
     int y;
+    int broken;
 
-    public Node(int x, int y) {
+    public Node(int x, int y, int broken) {
         this.x = x;
         this.y = y;
+        this.broken =  broken;
     }
 }
