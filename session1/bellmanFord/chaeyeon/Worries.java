15bd6d4085453b40f2ae107d8e9ea9f0f694cdde
inha0319@naver.com <inha031913@gmail.com>
2025-05-22 14:43:38 +0900

    fix : 코드 수정

--- session1/bellmanFord/chaeyeon/Worries.java
+++ session1/bellmanFord/chaeyeon/Worries.java
@@ -17,8 +17,8 @@ public class Worries {
     static int N, start, end, M;
     static List<Edge> edges = new ArrayList<>();
     static long[] dist;
-    static boolean[] reachable;
-    static int[] earn;
+    static boolean[] visited;
+    static int[] money;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -28,9 +28,6 @@ public class Worries {
         end = sc.nextInt();
         M = sc.nextInt();
 
-        dist = new long[N];
-        earn = new int[N];
-
         for (int i = 0; i < M; i++) {
             int from = sc.nextInt();
             int to = sc.nextInt();
@@ -38,65 +35,55 @@ public class Worries {
             edges.add(new Edge(from, to, cost));
         }
 
-        for (int i = 0; i < N; i++) {
-            earn[i] = sc.nextInt();
-            dist[i] = NEG_INF;
+        money  = new int[N];
+        for(int i=0; i<N;i++){
+            money[i] = sc.nextInt();
         }
 
-        dist[start] = earn[start];
+        dist = new long[N];
+        Arrays.fill(dist,NEG_INF);
+        dist[start] = money[start];
 
-        // 벨만 포드
+
+
         for (int i = 0; i < N - 1; i++) {
             for (Edge edge : edges) {
-                if (dist[edge.from] == NEG_INF) continue;
-                if (dist[edge.to] < dist[edge.from] - edge.cost + earn[edge.to]) {
-                    dist[edge.to] = dist[edge.from] - edge.cost + earn[edge.to];
+                if (dist[edge.from] == NEG_INF)
+                    continue;
+                if (dist[edge.to] < dist[edge.from] - edge.cost + money[edge.to]) {
+                    dist[edge.to] = dist[edge.from] - edge.cost + money[edge.to];
                 }
             }
         }
 
-        // 양의 사이클 탐지
-        boolean infinite = false;
-        for (int i = 0; i < N; i++) {
-            for (Edge edge : edges) {
-                if (dist[edge.from] == NEG_INF) continue;
-                if (dist[edge.to] < dist[edge.from] - edge.cost + earn[edge.to]) {
-                    if (isReachable(edge.to, end)) {
-                        infinite = true;
-                        break;
-                    }
+        for (Edge edge : edges) {
+            if (dist[edge.from] == NEG_INF)
+                continue;
+            if (dist[edge.to] < dist[edge.from] - edge.cost + money[edge.to]) {
+                visited = new boolean[N];
+                dfs(edge.to);
+                if (visited[end]) {
+                    System.out.println("Gee");
+                    return;
                 }
             }
-            if (infinite) break;
         }
+
 
         if (dist[end] == NEG_INF) {
             System.out.println("gg");
-        } else if (infinite) {
-            System.out.println("Gee");
-        } else {
+        }
+        else {
             System.out.println(dist[end]);
         }
     }
-
-    static boolean isReachable(int from, int target) {
-        boolean[] visited = new boolean[N];
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(from);
-        visited[from] = true;
-
-        while (!q.isEmpty()) {
-            int curr = q.poll();
-            if (curr == target) return true;
-
-            for (Edge edge : edges) {
-                if (edge.from == curr && !visited[edge.to]) {
-                    visited[edge.to] = true;
-                    q.offer(edge.to);
-                }
+    static void dfs(int node){
+        visited[node] = true;
+        for(Edge edge : edges){
+            if(edge.from == node && !visited[edge.to]){
+                dfs(edge.to);
             }
         }
 
-        return false;
     }
 }
