c35b28138c300b11c1bd0c1ed0d9a4149e017ba4
inha0319@naver.com <inha031913@gmail.com>
2025-05-26 10:39:48 +0900

    feat : 소수 찾기 (프로그래머스 - 42839)

---
+++ session1/backtracking/chaeyeon/FindPrime.java
@@ -0,0 +1,56 @@
+package backtracking.chaeyeon;
+
+import java.util.*;
+
+public class FindPrime {
+    static Set<Integer> result = new HashSet<>();
+    static boolean[] visited;
+
+    public int findPrime(String numbers) {
+        visited = new boolean[numbers.length()];
+        dfs(numbers, "",0);
+
+        int cnt = 0;
+        for(int num : result){
+            if (isPrime(num))
+                cnt++;
+        }
+        return cnt;
+
+    }
+
+    public void dfs(String numbers, String num, int depth){
+        if (!num.isEmpty()){
+            result.add(Integer.parseInt(num));
+        }
+
+        if(depth == numbers.length())
+            return;
+
+        for(int i=0;i<numbers.length(); i++){
+            if(!visited[i]){
+                visited[i] = true;
+                dfs(numbers, num+ numbers.charAt(i), depth+1);
+                visited[i] = false;
+            }
+        }
+    }
+    public boolean isPrime(int num){
+        if (num<=1)
+            return false;
+        if(num==2)
+            return true;
+        for(int i=2;i<=Math.sqrt(num); i++){
+            if (num%i==0)
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        FindPrime fp = new FindPrime();
+        String numbers = "17";
+        int result = fp.findPrime(numbers);
+        System.out.println(result);
+    }
+}
