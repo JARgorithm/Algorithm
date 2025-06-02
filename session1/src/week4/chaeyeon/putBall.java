3c072489277a8592dfe268cbcd8ec70a8ed0e163
inha0319@naver.com <inha031913@gmail.com>
2025-06-02 13:55:57 +0900

    feat : 공 넣기 (백준 - 10810)

---
+++ session1/src/week4/chaeyeon/putBall.java
@@ -0,0 +1,34 @@
+package week4.chaeyeon;
+
+import java.util.Scanner;
+
+public class putBall {
+    static int n;
+    static int m;
+    static int[] basket;
+
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        basket = new int[n + 1];
+
+        for (int a= 0; a < m; a++) {
+            int i = sc.nextInt();
+            int j = sc.nextInt();
+            int k = sc.nextInt();
+
+            for (int b = i; b<=j; b++){
+                basket[b] = k;
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(basket[i] + " ");
+        }
+
+
+    }
+}
