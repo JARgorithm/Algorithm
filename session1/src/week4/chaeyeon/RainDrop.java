f1eeebf1a91bd23b80de5d26f8156ee25371ec2b
inha0319@naver.com <inha031913@gmail.com>
2025-06-04 12:33:42 +0900

    feat : 빗물 (백준 - 14719)

---
+++ session1/src/week4/chaeyeon/RainDrop.java
@@ -0,0 +1,41 @@
+package week4.chaeyeon;
+
+import java.util.Scanner;
+
+public class RainDrop {
+    static int h;
+    static int w;
+    static int[] blocks;
+    static int cnt=0;
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        h = sc.nextInt();
+        w = sc.nextInt();
+        blocks=new int[w];
+
+        for(int i=0; i<w; i++){
+            blocks[i] = sc.nextInt();
+        }
+
+        for(int i=1;i<w;i++){
+            int leftMax = 0;
+            for(int j=0;j<i;j++){
+                leftMax = Math.max(leftMax, blocks[j]);
+            }
+
+            int rightMax = 0;
+            for(int j= i+1; j<w; j++){
+                rightMax = Math.max(rightMax,blocks[j]);
+            }
+
+            int water = Math.min(leftMax, rightMax) - blocks[i];
+            if(water>0){
+                cnt+=water;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
