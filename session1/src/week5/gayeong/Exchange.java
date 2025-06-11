96312835b4a06647004deb340f4b2c6a6b731a9c
Ga0Lee <leegy0419@gmail.com>
2025-06-12 01:47:28 +0900

    feat: 거스름돈 문제 풀이

---
+++ session1/src/week5/gayeong/Exchange.java
@@ -0,0 +1,16 @@
+package week5.gayeong;
+
+public class Exchange {
+    public int solution(int n, int[] money) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+
+        for (int coin : money) {
+            for (int i = coin; i <= n; i++) {
+                dp[i] += dp[i - coin];
+            }
+        }
+
+        return dp[n];
+    }
+}
