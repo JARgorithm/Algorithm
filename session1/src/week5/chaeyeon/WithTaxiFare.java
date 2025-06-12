e5e88c18a532e79f49d31cb0064bf1121bf8a007
inha0319@naver.com <inha031913@gmail.com>
2025-06-12 12:35:39 +0900

    feat : 카드구매하기 (백준 - 11052)

--- session1/src/week5/chaeyeon/WithTaxiFare.java
+++ session1/src/week5/chaeyeon/WithTaxiFare.java
@@ -27,7 +27,7 @@ public class WithTaxiFare {
         for (int k = 1; k <= n; k++) {
             for (int i = 1; i <= n; i++) {
                 for (int j = 1; j <= n; j++) {
-                    if (dist[i][k] != INF && dist[k][j] != INF) { 
+                    if (dist[i][k] != INF && dist[k][j] != INF) {
                         dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
                     }
                 }
